15e6699bc57e65b87e2732a8d5620c972bcc2922
abvol <abvol@mail.ru>
2025-11-02 13:26:02 +1000

    v4

---
+++ Test3.py
@@ -0,0 +1,13 @@
+
+
+try:
+    a = input("Введите число: ")
+    number = int(a)
+    if 100 <= number <= 999 and number%2==0 and number%3==0:
+        print(f"✓ Число {number} подходит")
+    else:
+        print(f"✗ Число {number} не подходит под условия")
+except ValueError:
+    print("✗ Это не целое число")
+
+
